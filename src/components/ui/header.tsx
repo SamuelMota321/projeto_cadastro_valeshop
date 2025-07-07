01aeac89b4cb023aed0a80b4d99f8e0c0f07623a
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2025-07-07 15:55:45 -0300

    valeshop logo

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,10 +1,10 @@
 export const Header = (): JSX.Element => {
   return (
-    <header className="w-full h-[70px] bg-gradient-to-r from-[#004075] to-[#00569E] flex items-center px-4">
+    <header className="w-full h-[100px] bg-gradient-to-r from-[#004075] to-[#00569E] flex items-center px-4">
       <img
-        className="h-[50px] object-contain"
+        className="h-[80px] object-contain"
         alt="Vale Shop Logo"
-        src="/rectangle-9.png"
+        src="../public/logo_valeshop.png"
       />
     </header>
   );
