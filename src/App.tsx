f406336cd50327bceb1543bb06cbafc6dd6707a8
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2025-07-08 16:54:35 -0300

    comentando também os routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { RouterProvider, createBrowserRouter, Navigate } from "react-router-dom";
-import { AlterarStatusCartao } from "./screens/AlterarStatusCartao";
-import { AlterarMatricula } from "./screens/AlterarMatricula";
+// import { AlterarStatusCartao } from "./screens/AlterarStatusCartao";
+// import { AlterarMatricula } from "./screens/AlterarMatricula";
 import { CadastrarUsuario } from "./screens/CadastrarUsuario";
-import { AlterarReferencia } from "./screens/AlterarReferenciaUsuario"
-import { CadastrarAlterarDadosBancarios } from "./screens/CadastrarAlterarDadosBancarios";
-import { NovoCadastroReferencia } from "./screens/NovoCadastroReferencia";
+// import { AlterarReferencia } from "./screens/AlterarReferenciaUsuario"
+// import { CadastrarAlterarDadosBancarios } from "./screens/CadastrarAlterarDadosBancarios";
+// import { NovoCadastroReferencia } from "./screens/NovoCadastroReferencia";
 
 
 const router = createBrowserRouter([
@@ -12,30 +12,30 @@ const router = createBrowserRouter([
     path: "/",
     element: <Navigate to="/alterar-status-cartao" replace />,
   },
-  {
-    path: "/alterar-status-cartao",
-    element: <AlterarStatusCartao />,
-  },
-  {
-    path: "/alterar-matricula",
-    element: <AlterarMatricula />,
-  },
+  // {
+  //   path: "/alterar-status-cartao",
+  //   element: <AlterarStatusCartao />,
+  // },
+  // {
+  //   path: "/alterar-matricula",
+  //   element: <AlterarMatricula />,
+  // },
   {
     path: "/cadastrar-usuario",
     element: <CadastrarUsuario />,
   },
-  {
-    path: "/alterar-referencia",
-    element: <AlterarReferencia />,
-  },
-  {
-    path: "/alterar-dados-bancarios",
-    element: <CadastrarAlterarDadosBancarios />,
-  },
-  {
-    path: "/novo-cadastro-referencia",
-    element: <NovoCadastroReferencia />,
-  },
+  // {
+  //   path: "/alterar-referencia",
+  //   element: <AlterarReferencia />,
+  // },
+  // {
+  //   path: "/alterar-dados-bancarios",
+  //   element: <CadastrarAlterarDadosBancarios />,
+  // },
+  // {
+  //   path: "/novo-cadastro-referencia",
+  //   element: <NovoCadastroReferencia />,
+  // },
 ]);
 
 export const App = () => {
